c3731c33eb0d24cbb86354296121103c77aa0e77
claytongentry <clayton.gentry@bustle.com>
2024-04-29 15:26:03 -0400

    Add formula

---
+++ Formula/manifolds.rb
@@ -0,0 +1,17 @@
+# in homebrew-manifolds/Formula/manifolds.rb
+class Manifolds < Formula
+  desc "CLI tool for managing data infrastructures"
+  homepage "https://github.com/bustle/manifolds"
+  url "https://rubygems.org/downloads/manifolds-0.0.1.gem"
+  sha256 "8304d96418d76a2cfa3a04d2267f15383cb5019fbc0359c43302839e055b9e97"
+  license "MIT"
+
+  def install
+    system "gem", "install", "manifolds-0.1.0.gem"
+    bin.install "exe/manifolds"
+  end
+
+  test do
+    system "#{bin}/manifolds", "--version"
+  end
+end
